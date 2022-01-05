49a129cf4f7b4556cf340f6fb12aaada551ce551
khyatidhaval <Khyati@iMac.broadband>
2022-01-05 17:40:11 +0000

    my first commit

---
+++ MyFirstAutomationProgram.java
@@ -0,0 +1,26 @@
+package com.myfirstautomationprogram;
+
+public class MyFirstAutomationProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Automation Testing");
+
+        System.out.print("A");
+        System.out.print(" journey");
+        System.out.print(" of");
+        System.out.print(" a");
+        System.out.print(" thousand");
+        System.out.print(" miles");
+        System.out.print( " begins");
+        System.out.print( "with");
+        System.out.print(" a");
+        System.out.print(" single");
+        System.out.print(" steps");
+        System.out.println();
+
+
+    }
+
+
+}
